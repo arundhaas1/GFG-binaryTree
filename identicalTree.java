389c08f885c9b171928c8391fb2c575392e6568f
Arun R <82589778+arundhaas1@users.noreply.github.com>
2022-02-09 21:53:27 +0530

    Update identicalTree.java

--- identicalTree.java
+++ identicalTree.java
@@ -1,6 +1,6 @@
     public static boolean equals(BinaryTree.Node node1, BinaryTree.Node node2){
         if(node1!=null && node2!=null){
-            if(node1.data!=node2.data){
+            if(node1.data!=node2.data){   //check every data values from top to bottom
                 return false;
             }else{
                 equals(node1.left,node2.left);
