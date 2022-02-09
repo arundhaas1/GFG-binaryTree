864620ae0859a6589bac07ba501b13126eadeb7f
Arun R <82589778+arundhaas1@users.noreply.github.com>
2022-02-09 21:51:10 +0530

    Create identicalTree.java

---
+++ identicalTree.java
@@ -0,0 +1,11 @@
+    public static boolean equals(BinaryTree.Node node1, BinaryTree.Node node2){
+        if(node1!=null && node2!=null){
+            if(node1.data!=node2.data){
+                return false;
+            }else{
+                equals(node1.left,node2.left);
+                equals(node1.right,node2.right);
+            }
+        }
+        return true;
+    }
