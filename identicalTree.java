1d40b0be987a30ec1f24693cbe0b49ef8794f394
Arun R <82589778+arundhaas1@users.noreply.github.com>
2022-02-09 22:56:35 +0530

    Update identicalTree.java

--- identicalTree.java
+++ identicalTree.java
@@ -1,26 +1,44 @@
-    public static boolean equals(BinaryTree.Node node1, BinaryTree.Node node2){
-        if(node1!=null && node2!=null){
-            if(node1.data!=node2.data){   //check every data values from top to bottom of left to left and right to right.
-                return false;
-            }else{
-                equals(node1.left,node2.left);   
-                equals(node1.right,node2.right);
-            }
+//IDENTICAL TREE..
+
+  public static boolean areIdenticalTrees(Node root1,Node root2){
+        if(root1 == null && root2 == null)  //if both reaches leaf node 
+            return true;
+        if(root1 == null || root2 == null)   //only one reaches the leaf node
+            return false;
+        if(root1 != null  && root2 != null) {
+            return ((root1.data == root2.data) &&
+                    (areIdenticalTrees(root1.left, root2.left)) &&
+                    (areIdenticalTrees(root1.right, root2.right)));
         }
-        return true;
+        return false;
     }
 
+//
+public class Main {
+    public static void main(String[] args) {
+	    Binarytree binarytree=new Binarytree(10);
+        binarytree.insertLeft(binarytree.root,5);
+        binarytree.insertRight(binarytree.root, 12);
+
+        Binarytree binarytree1=new Binarytree(10);
+        binarytree.insertLeft(binarytree1.root,5);
+        binarytree.insertRight(binarytree1.root, 12);
+
+        System.out.println(binarytree.areIdenticalTrees(binarytree.root, binarytree1.root));
+    }
+}
 
 //MIRROR TREE
 
-    public static boolean equals(BinaryTree.Node node1, BinaryTree.Node node2){
-        if(node1!=null && node2!=null){
-            if(node1.data!=node2.data){
-                return false;
-            }else{
-                equals(node1.left,node2.right);    //check every left value with right and every right value with left
-                equals(node1.right,node2.left);
-            }
+public static boolean areMirrorTrees(Node root1,Node root2){
+        if(root1 == null && root2 == null)
+            return true;
+        if(root1 == null || root2 == null)
+            return false;
+        if(root1 != null  && root2 != null) {
+            return ((root1.data == root2.data) &&
+                    (areMirrorTrees(root1.left, root2.right)) &&
+                    (areMirrorTrees(root1.right, root2.left)));
         }
-        return true;
+        return false;
     }
