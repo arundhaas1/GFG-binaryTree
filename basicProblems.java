5ccd9877be58879eb32c1c62a43bd788edef3b6d
Arun R <82589778+arundhaas1@users.noreply.github.com>
2022-02-15 20:35:24 +0530

    Update and rename MaxHeight.java to basicProblems.java

---
+++ basicProblems.java
@@ -0,0 +1,39 @@
+public int findHeight(Node root) {
+        if(root==null){
+            return 0;
+        }else{
+            int lh=height(root.left);
+            int rh=height(root.right);
+
+            return (lh>rh?lh+1:rh+1);
+        }
+    }
+
+//Number of Node
+
+public int findHeight(Node root) {
+         if(root==null){
+            return 0;
+        }else{
+            int lh=height(root.left);
+            int rh=height(root.right);
+
+            return lh+rh+1;
+        }
+    }
+
+//Number of Leaf Nodes
+
+public int findHeight(Node root) {
+         if(root==null){
+            return 0;
+        }else if(root.left==null && root.right==null){
+            return 1;
+        }else{
+            int lh=height(root.left);
+            int rh=height(root.right);
+
+            return (lh>rh?lh+1:rh+1);
+        }
+    }
+
