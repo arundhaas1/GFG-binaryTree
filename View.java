da5496ea93ecc9cccd0a4cab00e30f7cd1bb3184
Arun R <82589778+arundhaas1@users.noreply.github.com>
2022-02-19 20:09:47 +0530

    Update View.java

--- View.java
+++ View.java
@@ -94,3 +94,74 @@ class BinaryTree {
 		leftView(node.left, level + 1);
 	}
 }
+
+//BOTTOM VIEW 
+
+package com.dhas;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+class Main{
+	public static void main(String args[]) {
+		BinaryTree tree = new BinaryTree();
+
+		tree.root = new Node(20);
+		tree.root.left = new Node(8);
+		tree.root.right = new Node(22);
+		tree.root.left.left = new Node(5);
+		tree.root.left.right = new Node(3);
+		tree.root.right.left = new Node(4);
+		tree.root.right.right = new Node(25);
+		tree.root.left.right.left = new Node(10);
+		tree.root.left.right.right = new Node(14);
+
+		tree.bottomView();
+	}
+}
+class Node{
+	int data;
+	int hd;
+	Node left, right;
+
+	public Node(int x){
+		data = x;
+		hd=Integer.MAX_VALUE;
+		left = right = null;
+	}
+}
+class BinaryTree{
+	Node root;
+	public void bottomView(){
+		if (root == null)
+			return;
+		int hd = 0;
+
+		TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+
+		Queue<Node> queue = new LinkedList<>();
+		root.hd = hd;
+		queue.add(root);
+
+		while (!queue.isEmpty()){
+			Node temp = queue.remove();
+			hd = temp.hd;
+
+			treeMap.put(hd, temp.data);
+
+			if (temp.left != null){
+				temp.left.hd = hd-1;
+				queue.add(temp.left);
+			}
+			if (temp.right != null){
+				temp.right.hd = hd+1;
+				queue.add(temp.right);
+			}
+		}
+		//Printing map
+		for(Map.Entry<Integer,Integer> entry:treeMap.entrySet()){
+			System.out.println(entry.getValue());
+		}
+	}
+}
